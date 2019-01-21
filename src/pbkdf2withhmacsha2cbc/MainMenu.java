5b69bd56dfde9399c44a9cb9c953e90005b7699e
Illestar Wu <mouse.schoolwork@gmail.com>
2019-01-21 15:38:23 -0800

    sample try

--- src/pbkdf2withhmacsha2cbc/MainMenu.java
+++ src/pbkdf2withhmacsha2cbc/MainMenu.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pbkdf2withhmacsha2cbc;
 
+import java.security.Security;
+import java.util.Scanner;
 /**
  *
  * @author Illestar
@@ -15,7 +13,32 @@ public class MainMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String password = "";
+        int enc_mode = 0;
+        
+        
+        // Debug: check Bouncy Castle provider;
+        if (Security.getProvider("BC") == null) {
+            System.out.println("Bouncy Castle provider is NOT available");
+        } else {
+            System.out.println("Bouncy Castle provider is available");
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select encryption mode: ");
+        enc_mode = sc.nextInt();
+        
+        System.out.println("Provide a password: ");
+        password = sc.next();
+        
+        byte[] pwd = password.getBytes();
+        
+        
+        
+        // close scanner 
+        sc.close();
+        
         
     }
     
