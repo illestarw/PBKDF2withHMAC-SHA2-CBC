ec9a655e6e801b8a3a44904b266fc00f0ec5347d
Illestar Wu <mouse.schoolwork@gmail.com>
2019-01-22 22:49:52 -0800

    unit test of encrypt

--- src/pbkdf2withhmacsha2cbc/Launch.java
+++ src/pbkdf2withhmacsha2cbc/Launch.java
@@ -5,16 +5,25 @@
  */
 package pbkdf2withhmacsha2cbc;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 /**
  *
  * @author Illestar
  */
 public class Launch {
     
-    public static void main(String args[])
+    public static void main(String args[]) throws GeneralSecurityException, IOException
     {
         CryptoLib cl = new CryptoLib(new CryptoInstance(CryptoInstance.Algorithm.AES, CryptoInstance.Mode.CBC, CryptoInstance.Padding.PKCS5_PADDING, CryptoInstance.KeyLength.BITS_256, CryptoInstance.Pbkdf.PBKDF_2_WITH_HMAC_SHA_512, CryptoInstance.MacAlgorithm.HMAC_SHA_512, 16, 1));
         
+        byte[] encrypted = cl.encrypt("in".getBytes(), "pass".toCharArray());
+        
+        for(byte b : encrypted){
+            System.out.print(b);
+        }
+        System.out.println();
     }
     
 }
