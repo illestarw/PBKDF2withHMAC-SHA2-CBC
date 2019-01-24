3841aac913622106005c460a0203a70403dd818d
Illestar Wu <mouse.schoolwork@gmail.com>
2019-01-23 20:34:28 -0800

    increase menu #1

--- src/pbkdf2withhmacsha2cbc/Launch.java
+++ src/pbkdf2withhmacsha2cbc/Launch.java
@@ -7,6 +7,7 @@ package pbkdf2withhmacsha2cbc;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
 
 /**
  *
@@ -16,14 +17,30 @@ public class Launch {
     
     public static void main(String args[]) throws GeneralSecurityException, IOException
     {
-        CryptoLib cl = new CryptoLib(new CryptoInstance(CryptoInstance.Algorithm.AES, CryptoInstance.Mode.CBC, CryptoInstance.Padding.PKCS5_PADDING, CryptoInstance.KeyLength.BITS_256, CryptoInstance.Pbkdf.PBKDF_2_WITH_HMAC_SHA_512, CryptoInstance.MacAlgorithm.HMAC_SHA_512, 16, 1));
+        // Init
         
-        byte[] encrypted = cl.encrypt("in".getBytes(), "pass".toCharArray());
         
-        for(byte b : encrypted){
-            System.out.print(b);
+        Scanner sc = new Scanner(System.in);
+        
+        while(true)
+        {
+            // Show menu
+            System.out.println("============= Encryption Module =============");
+            System.out.println("Select the algorithm for encryption:");
+            
+            
+            CryptoLib cl = new CryptoLib(new CryptoInstance(CryptoInstance.Algorithm.AES, CryptoInstance.Mode.CBC, CryptoInstance.Padding.PKCS5_PADDING, CryptoInstance.KeyLength.BITS_256, CryptoInstance.Pbkdf.PBKDF_2_WITH_HMAC_SHA_512, CryptoInstance.MacAlgorithm.HMAC_SHA_512, 16, 1));
+
+        
+        
+            byte[] encrypted = cl.encrypt("in".getBytes(), "pass".toCharArray());
+
+            for(byte b : encrypted){
+                System.out.print(b);
+            }
+            System.out.println();
         }
-        System.out.println();
+        
     }
     
 }
