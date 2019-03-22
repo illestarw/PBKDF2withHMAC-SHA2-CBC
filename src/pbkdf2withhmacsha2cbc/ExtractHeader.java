0e287d326a3e223b0a2680da01b9370160e92adb
Illestar Wu <mouse.schoolwork@gmail.com>
2019-03-21 23:13:46 -0700

    Update ExtractHeader.java

--- src/pbkdf2withhmacsha2cbc/ExtractHeader.java
+++ src/pbkdf2withhmacsha2cbc/ExtractHeader.java
@@ -57,18 +57,25 @@ public class ExtractHeader {
         } else if (headerString.charAt(0) == 'D') {
             cis.setAlgorithm(CryptoInstance.Algorithm.DESede);
             cis.setIvLength(8);
+        } else {
+            throw new IllegalArgumentException("Input file is not a valid file encrypted by this program.");
         }
 
         if (Integer.parseInt(headerString.substring(1, 4)) == 128) cis.setKeyLength(CryptoInstance.KeyLength.BITS_128);
         else if (Integer.parseInt(headerString.substring(1, 4)) == 192) cis.setKeyLength(CryptoInstance.KeyLength.BITS_192);
         else if (Integer.parseInt(headerString.substring(1, 4)) == 256) cis.setKeyLength(CryptoInstance.KeyLength.BITS_256);
-
+        else {
+            throw new IllegalArgumentException("Input file is not a valid file encrypted by this program.");
+        }
+        
         if (Integer.parseInt(headerString.substring(4, 7)) == 256) {
             cis.setPbkdf(CryptoInstance.Pbkdf.PBKDF_2_WITH_HMAC_SHA_256);
             cis.setMacAlgorithm(CryptoInstance.MacAlgorithm.HMAC_SHA_256);
         } else if (Integer.parseInt(headerString.substring(4, 7)) == 512) {
             cis.setPbkdf(CryptoInstance.Pbkdf.PBKDF_2_WITH_HMAC_SHA_512);
             cis.setMacAlgorithm(CryptoInstance.MacAlgorithm.HMAC_SHA_512);
+        } else {
+            throw new IllegalArgumentException("Input file is not a valid file encrypted by this program.");
         }
 
         cis.setIterations(Integer.parseInt(headerString.substring(headerString.length() - 9)));
